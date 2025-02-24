3d99163c0bcd86a19f1916304521ed1ce8ddcea0
Dell <Dell@DESKTOP-1F6GPQ8>
2025-02-24 12:20:37 +0530

    booking function for the customer

---
+++ src/main/java/com/cab/model/Booking.java
@@ -0,0 +1,29 @@
+package com.cab.model;
+
+public class Booking {
+    private String orderNumber;
+    private String customerName;
+    private String address;
+    private String telephone;
+    private String destination;
+    private String bookingDate;
+    private String bookingTime;
+
+    public Booking(String orderNumber, String customerName, String address, String telephone, String destination, String bookingDate, String bookingTime) {
+        this.orderNumber = orderNumber;
+        this.customerName = customerName;
+        this.address = address;
+        this.telephone = telephone;
+        this.destination = destination;
+        this.bookingDate = bookingDate;
+        this.bookingTime = bookingTime;
+    }
+
+    public String getOrderNumber() { return orderNumber; }
+    public String getCustomerName() { return customerName; }
+    public String getAddress() { return address; }
+    public String getTelephone() { return telephone; }
+    public String getDestination() { return destination; }
+    public String getBookingDate() { return bookingDate; }
+    public String getBookingTime() { return bookingTime; }
+}
